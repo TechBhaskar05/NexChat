458dfebc70f104b8904964a9b5d0713f08c420a9
Pratyush Bhaskar <pratyush.bhaskar08@gmail.com>
2025-06-16 01:50:13 +0530

    feat: implement message handling routes and controller functions

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './lib/db.js';
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config({ path: "./.env"});
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 connectDB()
 .then(() => {
