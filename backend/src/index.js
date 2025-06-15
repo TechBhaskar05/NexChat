d9f00e2834c2925f522c0fb887beefd9308a89f8
Pratyush Bhaskar <pratyush.bhaskar08@gmail.com>
2025-06-16 01:30:14 +0530

    feat: initialize frontend with Vite and React setup
    - Added package.json with dependencies for React, Vite, and ESLint.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the application.
    - Developed App.jsx as the main component with state management.
    - Added React logo SVG in the assets directory.
    - Established global styles in index.css.
    - Set up main entry point in main.jsx for rendering the application.
    - Configured Vite with a basic vite.config.js file.

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import connectDB from './lib/db.js';
+import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+
+dotenv.config({ path: "./.env"});
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERRR: ", error);
+        throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
